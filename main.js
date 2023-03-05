de2d06e7221f2904682e39eec64d3a099b85277c
Timofey Bashkatov <tima0911@bk.ru>
2023-03-05 15:33:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var a = 2;
+var b = 3;
+var c = 4;
+var d = 5;
+/*Calculations with a & b*/
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a**b);
+console.log(a/b);
+console.log(a%b);
+/*Calculations with c & d*/
+console.log(c+d);
+console.log(c-d);
+console.log(c*d);
+console.log(c**d);
+console.log(c/d);
+console.log(c%d);
+/* Alert */
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a**b);
+alert(a/b);
+alert(a%b);
+/* Prompt */
+prompt("Введи своё имя.")
+prompt("Введи свою фамилию.")
+prompt("Введи свой возраст.")

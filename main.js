3188caa4ed2ccaaca0e9fc3e97fee6eab901c481
Timofey Bashkatov <tima0911@bk.ru>
2023-03-12 15:13:45 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
-var a = 2;
-var b = 3;
-var c = 4;
-var d = 5;
-/*Calculations with a & b*/
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a**b);
-console.log(a/b);
-console.log(a%b);
-/*Calculations with c & d*/
-console.log(c+d);
-console.log(c-d);
-console.log(c*d);
-console.log(c**d);
-console.log(c/d);
-console.log(c%d);
-/* Alert */
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a**b);
-alert(a/b);
-alert(a%b);
-/* Prompt */
-prompt("Введи своё имя.")
-prompt("Введи свою фамилию.")
-prompt("Введи свой возраст.")
+var a = Number(prompt("1 число:"));
+var b = Number(prompt("2 число:"));
+var operation = prompt("Действие:");
+if (operation == "+") {
+    alert(a+b);
+}
+if (operation == "-") {
+    alert(a-b);
+}
+if (operation == "*") {
+    alert(a*b);
+}
+if (operation == "/") {
+    alert(a/b);
+}
+else{
+    alert("ERROR");  
+}
